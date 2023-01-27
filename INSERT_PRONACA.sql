ef9e48379140f328948a296af8b148168a9c2d58
elvisdavid21 <79238237+elvisdavid21@users.noreply.github.com>
2023-01-27 11:48:57 -0500

    //Update - Script Insert

--- INSERT_PRONACA.sql
+++ INSERT_PRONACA.sql
@@ -24,7 +24,7 @@ INSERT INTO `estado_produccion`(`ESTADO_PRODUCCION`, `DETALLE_ESTADO_PRODU`)
 VALUES ('Finalizado','El producto esta listo para la venta al cliente');
 
 INSERT INTO `estado_produccion`(`ESTADO_PRODUCCION`, `DETALLE_ESTADO_PRODU`) 
-VALUES ('Sin Stock','Este producto se encuentra agotado')
+VALUES ('Sin Stock','Este producto se encuentra agotado');
 
 /*Tipo Item*/
 INSERT INTO `tipo_item`(`TIPO_ITEM`, `DETALLE_TIPO_ITEM`) 
@@ -108,12 +108,12 @@ INSERT INTO `item`(`ID_LISTA_ITEMS`, `ID_ESTADO_PRODUCION`, `ID_TIPO_ITEM`, `COD
 VALUES (1,1,3,'101010','MORTADELA PLUMROSE','2023-01-26','2023-02-26','2023-03-26','LT1010',100,1.5,200,'Refrigerado',
         'Linea Diaria');
 
-/*Consulta productos terminados para ventas*/
+/*Consulta productos terminados para ventas
 SELECT ITEM.ID_ITEM, ITEM.ID_LISTA_ITEMS, ITEM.ID_ESTADO_PRODUCION, ITEM.ID_TIPO_ITEM, ITEM.CODIGO_ITEM, ITEM.NOMBRE_ITEM,
 ITEM.FECHA_FABRI_ITEM, ITEM.FECHA1_CADU_ITEM, ITEM.FECHA2_CADU_ITEM, ITEM.LOTE_ITEM, ITEM.CANTIDAD_LOTE_ITEM, ITEM.PRECIO_ITEM,
 ITEM.PESO_ITEM, ITEM.CONSERVACION_ITEM, ITEM.DETALLE_ITEM FROM `item`,`tipo_item`,`estado_produccion` 
 WHERE item.ID_ESTADO_PRODUCION=estado_produccion.ID_ESTADO_PRODUCCION AND item.ID_TIPO_ITEM=tipo_item.ID_TIPO_ITEM AND
-tipo_item.TIPO_ITEM='Producto' AND estado_produccion.ESTADO_PRODUCCION='Finalizado'
+tipo_item.TIPO_ITEM='Producto' AND estado_produccion.ESTADO_PRODUCCION='Finalizado';*/
 
 CREATE PROCEDURE LIST_PRODUCTOS()
 SELECT ITEM.ID_ITEM, ITEM.ID_LISTA_ITEMS, ITEM.ID_ESTADO_PRODUCION, ITEM.ID_TIPO_ITEM, ITEM.CODIGO_ITEM, ITEM.NOMBRE_ITEM,
