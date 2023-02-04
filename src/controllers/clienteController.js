d710fcc3722c8d62d3b9e476b5a0b84720a14525
José Pallo <jose.pallo.md@gmail.com>
2023-02-04 16:29:18 -0500

    Actualización Consulta de Clientes

--- src/controllers/clienteController.js
+++ src/controllers/clienteController.js
@@ -18,7 +18,9 @@ const database_1 = __importDefault(require("../database"));
 class ClienteController {
     list(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
-            const cliente = yield database_1.default.query('SELECT * FROM cliente_potencial');
+            const consulta = 
+            "SELECT cliente.NOMBRE_CLIENTE,cliente.APELLIDO_CLIENTE,cliente.RUC_CEDULA_,cliente.EMAIL_CLIENTE,cliente.ESTADO_CLIENTE,ubicacion.ZONA_UBICACION,ubicacion.SECTOR_UBICACION,ubicacion.NOM_UBICACION FROM cliente JOIN ubicacion ON ubicacion.ID_UBICACION=cliente.ID_UBICACION;"       
+            const cliente = yield database_1.default.query(consulta);
             res.json(cliente);
         });
     }
